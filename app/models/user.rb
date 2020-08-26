dda0c427cb513815acf203a404e6cd2aec94b86e
HANYA3106 <65996959+HANYA3106@users.noreply.github.com>
2020-08-26 21:13:01 +0900

    ユーザー管理機能の実装２

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
     validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     nickname { Faker::Name.last_name }
     email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
-    password {password}
-    password_confirmation {password}
+    password { password }
+    password_confirmation { password }
     first_name { Gimei.name.first }
     family_name { Gimei.name.last }
     first_name_kana { Gimei.first.katakana }
