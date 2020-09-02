a5dd15ea960b8591457a7227f5fa126bff066234
HANYA3106 <65996959+HANYA3106@users.noreply.github.com>
2020-09-02 12:12:40 +0900

    payjp 実装

---
+++ app/javascript/card.js
@@ -0,0 +1,39 @@
+const pay = () => {
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);  // PAY.JPテスト公開鍵
+  //環境変数から公開鍵を取得するようにしている→直接公開鍵を付与してあげる
+  const form = document.getElementById("charge-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("number"), //name属性がnumberになっているかどうか
+      exp_month: formData.get("exp_month"),
+      exp_year: `20${formData.get("exp_year")}`,
+      cvc: formData.get("cvc"),
+    };
+    console.log(card) // number: null, ..., cvc: null
+
+    Payjp.createToken(card, (status, response) => {
+      if (status === 200) {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} type="hidden" name='token'>`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
+
+        document.getElementById("card-number").removeAttribute("name");
+        document.getElementById("card-cvc").removeAttribute("name");
+        document.getElementById("card-exp-month").removeAttribute("name");
+        document.getElementById("card-exp-year").removeAttribute("name");
+
+        document.getElementById("charge-form").submit();
+        document.getElementById("charge-form").reset();
+      } else {
+      }
+    });
+  });
+};
+
+window.addEventListener("load", pay);
